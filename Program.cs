5717c69a7e89f894fd7119d90b7a40bc6a115583
1w4w <walter@walter.nu>
2026-09-24 16:14:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzzNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
+            int n = int.Parse(input[2]);
+            FizzBuzz(x, y, n);
+        }
+
+        public static void FizzBuzz(int x, int y, int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % x == 0 && i % y == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % x == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % y == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
